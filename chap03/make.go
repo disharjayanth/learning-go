a09647dcd43ab0d004a72fdc69564d6cbcf1c67d
disharjayantha <dishuj15@gmail.com>
2021-04-29 16:32:31 +0530

    declaring different ways in slice

--- chap03/make.go
+++ chap03/make.go
@@ -11,4 +11,33 @@ func main() {
 	fmt.Println("Length of z:", len(z), "Capacity of z:", cap(z))
 	z = append(z, 10, 20, 30, 40)
 	fmt.Println("z:", z)
+
+	// Declaring slice
+	var a []int
+	fmt.Println("a slice:", a)
+	if a == nil {
+		fmt.Println("a slice is nil....")
+	}
+
+	b := []int{}
+	fmt.Println("b slice:", b)
+	if b == nil {
+		fmt.Println("b slice is nil....")
+	}
+
+	c := make([]int, 4)
+	fmt.Println("c slice:", c)
+	if c == nil {
+		fmt.Println("c is nil....")
+	}
+
+	d := make([]int, 0, 10)
+	d = append(d, 0)
+	fmt.Println("d slice:", d, "length of d slice:", len(d), "capacity of d slice:", cap(d))
+
+	d = append(d, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+	fmt.Println("d slice:", d, "length of d slice:", len(d), "capacity of d slice:", cap(d))
+
+	d = append(d, 11)
+	fmt.Println("d slice:", d, "length of d slice:", len(d), "capacity of d slice:", cap(d))
 }
