5bf3b3dcea5d0c35e0ba2b13a6233d84316df247
disharjayantha <dishuj15@gmail.com>
2021-04-30 00:26:13 +0530

    slice confusing stuff

--- chap03/slice.go
+++ chap03/slice.go
@@ -21,16 +21,54 @@ func main() {
 	fmt.Println("Capacity of a:", cap(a))
 
 	// Capacity of slice
-	var x []int
-	fmt.Println(x, "Length:", len(x), "Capacity:", cap(x))
-	x = append(x, 10)
-	fmt.Println(x, "Length:", len(x), "Capacity:", cap(x))
-	x = append(x, 20)
-	fmt.Println(x, "Length:", len(x), "Capacity:", cap(x))
-	x = append(x, 30)
-	fmt.Println(x, "Length:", len(x), "Capacity:", cap(x))
-	x = append(x, 40)
-	fmt.Println(x, "Length:", len(x), "Capacity:", cap(x))
-	x = append(x, 50)
-	fmt.Println(x, "Length:", len(x), "Capacity:", cap(x))
+	// var x []int
+	// fmt.Println(x, "Length:", len(x), "Capacity:", cap(x))
+	// x = append(x, 10)
+	// fmt.Println(x, "Length:", len(x), "Capacity:", cap(x))
+	// x = append(x, 20)
+	// fmt.Println(x, "Length:", len(x), "Capacity:", cap(x))
+	// x = append(x, 30)
+	// fmt.Println(x, "Length:", len(x), "Capacity:", cap(x))
+	// x = append(x, 40)
+	// fmt.Println(x, "Length:", len(x), "Capacity:", cap(x))
+	// x = append(x, 50)
+	// fmt.Println(x, "Length:", len(x), "Capacity:", cap(x))
+
+	x := []int{1, 2, 3, 4}
+	fmt.Println("x slice:", x)
+	y := x[:2]
+	fmt.Println("y slice from index 0 upto 2:", y)
+	z := x[1:]
+	fmt.Println("z slice from index 1 to end:", z)
+	g := x[1:3]
+	fmt.Println("g slice from index 1 upto 3:", g)
+	h := x[:]
+	fmt.Println("h slice from start to end:", h)
+
+	i := []int{1, 2, 3, 4}
+	j := i[:2]
+	k := i[1:]
+	i[1] = 20
+	j[0] = 10
+	k[1] = 30
+	fmt.Println("i:", i)
+	fmt.Println("j:", j)
+	fmt.Println("k:", k)
+
+	// Confusing
+	// Slicing slice while apending
+	l := []int{1, 2, 3, 4}
+	m := l[:2]
+	fmt.Println("Cap:", cap(l), cap(m))
+	m = append(m, 30)
+	fmt.Println("l:", l)
+	fmt.Println("m:", m)
+
+	// More Confusing
+	r := make([]int, 0, 5)
+	r = append(r, 1, 2, 3, 4)
+	s := r[:2] // 1, 2
+	t := r[2:] // 3, 4
+	fmt.Println("r:", cap(r), "s:", cap(s), "t:", cap(t))
+
 }
