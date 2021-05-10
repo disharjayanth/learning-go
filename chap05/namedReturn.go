ff4e5d8383b9f0d3ee94dcd95b0e694fc268bc93
disharjayantha <dishuj15@gmail.com>
2021-05-10 20:08:27 +0530

    Changes to named return example

--- chap05/namedReturn.go
+++ chap05/namedReturn.go
@@ -9,8 +9,16 @@ func main() {
 	res, remainder, err := divAndRemainder(4, 2)
 	if err != nil {
 		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Dividend:", res, "Remainder:", remainder)
 	}
-	fmt.Println("Dividend:", res, "Remainder:", remainder)
+
+	res, remainder, err = divAndRemainder(0, 2)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Dividend:", res, "Remainder:", remainder)
+	}
 }
 
 func divAndRemainder(numerator int, denominator int) (result int, remainder int, err error) {
@@ -18,7 +26,5 @@ func divAndRemainder(numerator int, denominator int) (result int, remainder int,
 		err = errors.New("Cannot divide by 0.")
 		return
 	}
-	result = numerator / denominator
-	remainder = numerator % denominator
-	return
+	return numerator / denominator, numerator % denominator, nil
 }
