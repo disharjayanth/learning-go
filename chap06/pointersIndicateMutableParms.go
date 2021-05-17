1eb336c5809f2c24fb2f67951a5d6ea3420a8dd0
disharjayantha <dishuj15@gmail.com>
2021-05-17 19:57:34 +0530

    pointer params are copies of passed args'

--- chap06/pointersIndicateMutableParms.go
+++ chap06/pointersIndicateMutableParms.go
@@ -1,23 +1,44 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var f *int
+// 	failedUpdate(f)
+// 	fmt.Printf("%T\t%v\n", f, f)
+// 	var i *int
+// 	a := 100
+// 	i = &a
+// 	successUpdate(i)
+// 	fmt.Println(*i)
+// }
+
+// func failedUpdate(f *int) {
+// 	x := 10
+// 	f = &x
+// }
+
+// func successUpdate(i *int) {
+// 	*i = 200
+// }
+
 package main
 
 import "fmt"
 
+func failedUpdate(px *int) {
+	x2 := 20
+	px = &x2
+}
+
+func successUpdate(px *int) {
+	*px = 20
+}
+
 func main() {
-	var f *int
-	failedUpdate(f)
-	fmt.Printf("%T\t%v\n", f, f)
-	var i *int
-	a := 100
-	i = &a
-	successUpdate(i)
-	fmt.Println(*i)
-}
-
-func failedUpdate(f *int) {
 	x := 10
-	f = &x
-}
-
-func successUpdate(i *int) {
-	*i = 200
+	failedUpdate(&x)
+	fmt.Println("failedUpdate:", x)
+	successUpdate(&x)
+	fmt.Println("successUpdate:", x)
 }
