34e968bfa1ee1e031d69878e063c91749e0e0832
disharjayantha <dishuj15@gmail.com>
2021-06-02 19:09:00 +0530

    internal package gives access to parent and sibling

---
+++ chap09/internal_package/foo/foo.go
@@ -0,0 +1,11 @@
+package foo
+
+import (
+	"fmt"
+
+	"github.com/disharjayanth/learningGo/chap09/internal_package/foo/internal"
+)
+
+func PrintDoubler() {
+	fmt.Println(internal.Doubler(2))
+}
---
+++ chap09/internal_package/foo/sibling/sibling.go
@@ -0,0 +1,11 @@
+package sibling
+
+import (
+	"fmt"
+
+	"github.com/disharjayanth/learningGo/chap09/internal_package/foo/internal"
+)
+
+func PrintDoubler() {
+	fmt.Println(internal.Doubler(2))
+}
