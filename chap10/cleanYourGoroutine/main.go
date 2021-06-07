a88e2dbdeda37aa03f595dcfff07ed8ec30b0cc7
disharjayantha <dishuj15@gmail.com>
2021-06-07 14:46:35 +0530

    something...

--- chap10/cleanYourGoroutine/main.go
+++ chap10/cleanYourGoroutine/main.go
@@ -2,20 +2,22 @@ package main
 
 import "fmt"
 
-func countTo(max int) <-chan int {
+func countTo(max int, done chan<- bool) <-chan int {
 	ch := make(chan int)
 	go func() {
 		for i := 0; i <= max; i++ {
 			ch <- i
 		}
 		close(ch)
-		fmt.Println("goroutine exited....")
+		close(done)
 	}()
 	return ch
 }
 
 func main() {
-	for val := range countTo(10) {
+	done := make(chan bool)
+	for val := range countTo(10, done) {
 		fmt.Println(val)
 	}
+	<-done
 }
