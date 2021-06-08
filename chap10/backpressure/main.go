f696c9ca095208e89bb26499cfa77cd960b9171e
disharjayantha <dishuj15@gmail.com>
2021-06-08 11:56:46 +0530

    backpressure

--- chap10/backpressure/main.go
+++ chap10/backpressure/main.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+)
 
+type PressureGauge struct {
+	ch chan struct{}
+}
+
+func New(limit int) *PressureGauge {
+	ch := make(chan struct{}, limit)
+	for i := 0; i < limit; i++ {
+		ch <- struct{}{}
+	}
+	return &PressureGauge{
+		ch: ch,
+	}
+}
+
+func (pg *PressureGauge) Process(f func()) error {
+	select {
+	case <-pg.ch:
+		f()
+		pg.ch <- struct{}{}
+		return nil
+	default:
+		return errors.New("no more capacity")
+	}
+}
+
+func doThingThatShouldBeLimited() string {
+	time.Sleep(2 * time.Second)
+	return "done"
+}
+
+func main() {
+	pg := New(10)
+	http.HandleFunc("/request", func(w http.ResponseWriter, r *http.Request) {
+		err := pg.Process(func() {
+			w.Write([]byte(doThingThatShouldBeLimited()))
+		})
+		if err != nil {
+			w.WriteHeader(http.StatusTooManyRequests)
+			w.Write([]byte("Too many request!"))
+		}
+	})
+
+	fmt.Println("Listing @PORT: 3000")
+	http.ListenAndServe(":3000", nil)
 }
